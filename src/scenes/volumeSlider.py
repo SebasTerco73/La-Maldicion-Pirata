bd905671e7810cce0851f20f941a14f6b0223dfc
Ironicocos <131069303+Ironicocos@users.noreply.github.com>
2025-10-17 16:52:07 -0300

    Update volumeSlider.py

--- src/scenes/volumeSlider.py
+++ src/scenes/volumeSlider.py
@@ -6,8 +6,8 @@ class Volume(Scene):
     def __init__(self, screen):
         super().__init__(screen)
         self.screen = screen
-        self.sliderX = 100
-        self.sliderY = 100
+        self.sliderX = 500
+        self.sliderY = 500
         self.clock = pygame.time.Clock()
         self.sliderWidth = 200
         self.sliderHeight = 10
@@ -38,13 +38,14 @@ class Volume(Scene):
                 if event.key == pygame.K_RIGHT:
                     self.volume = self.volume + 0.1
                     pygame.mixer.music.set_volume(self.volume)
-                    self.draw()
+                    self.actualVolume = self.sliderX + int(self.volume * self.sliderWidth)
                     print(self.volume)
                 if event.key == pygame.K_LEFT:
                     self.volume = self.volume - 0.1
                     pygame.mixer.music.set_volume(self.volume)
-                    self.draw()
+                    self.actualVolume = self.sliderX + int(self.volume * self.sliderWidth)
                     print(self.volume)
+
     def handleVolume(self):
         if self.volume >= 1.0:
             self.volume = 1
@@ -52,21 +53,21 @@ class Volume(Scene):
             self.volume = 0.0
     def draw(self):
         self.screen.fill((250, 250, 250))
-        pygame.draw.rect(self.screen, (120, 120, 120),
+        pygame.draw.rect(self.screen, (250, 0, 0),
         (self.sliderX, self.sliderY - self.sliderHeight // 2, self.sliderWidth - self.sliderHeight, self.sliderHeight))
-        pygame.draw.rect(self.screen, (0, 0, 0), 
+        pygame.draw.rect(self.screen, (0, 0, 250), 
         (self.sliderX, self.sliderY - self.sliderHeight // 2, self.actualVolume - self.sliderX, self.sliderHeight))
         pygame.draw.rect(self.screen, (220, 220, 220), 
         (self.actualVolume, self.sliderY, self.knobRadius, self.sliderHeight))
+        
     def run(self):
         running = True
         while running:
             pressedKey = self.handleEvents()
             print(self.volume)
             self.draw()
-            self.handleEvents()
+            self.handleVolume()
             pygame.display.update((500, 500, self.sliderWidth, self.sliderHeight))
             self.clock.tick(FPS)
-            self.handleVolume()
             if pressedKey:
-                running = False 
+                running = False
