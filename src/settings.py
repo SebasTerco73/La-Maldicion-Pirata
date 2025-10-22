6dfd78073d5f63cb0ffecdb08a3e5d0b11263073
Ironicocos <131069303+Ironicocos@users.noreply.github.com>
2025-10-22 19:40:53 -0300

    Add files via upload

--- src/settings.py
+++ src/settings.py
@@ -8,13 +8,22 @@ IMAGES = {
 
 IMAGES_MENU = {
     "menu_bg": "assets/images/menu_bg.png",
+    "menu_bg_en": "assets/images/menu_bg_en.png",
+    "menu_bg_zh": "assets/images/menu_bg_zh.png",
 }
 
 IMAGES_LVL1 = {
-    "level1_bg": "assets/images/lvl1.jpg",
     "enemy_crab": "assets/images/cangrejo.png",
+    "bg_far": "assets/images/bg_far.PNG",
+    "bg_middle": "assets/images/bg_middle.PNG",
+    "bg_near": "assets/images/bg_near.PNG",
 }
 
+IMAGES_LVL2= {
+    "bg_far": "assets/images/lvl2_bg.png",
+    "bg_middle": "assets/images/lvl2_bg2.png",
+    "bg_near": "assets/images/lvl2_bg2.png",
+}
 # --- Sonidos ---
 SOUNDS_MENU = {
     "menu_music": "assets/sounds/menu_sound.mp3",
@@ -24,7 +33,8 @@ SOUNDS_MENU = {
 }
 
 SOUNDS_LVL1 = {
-    "level1_sound": "assets/sounds/lvl1_sound.mp3",
+    "level1_sound": "assets/sounds/lvl1_sound_LLuvia.mp3",
+    "lvl1_sound_Truenos": "assets/sounds/lvl1_sound_Trueno.mp3"
 }
 
 FONTS = {
@@ -40,5 +50,5 @@ BLUE = (0, 0, 255)
 RED = (120, 6, 6)
 MENU_FONT_SIZE = 40
 MENU_MARGIN = 20
-
+LANGUAGE = "es" 
 LVL1_GROUND_Y = 500
